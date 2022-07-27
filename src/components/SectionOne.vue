9ddd874bb010dd6efb48da4f527ec4a75635335b
Vito Farenga <vitofarenga@libero.it>
2022-07-27 17:04:59 +0200

    bottone fatto

--- src/components/SectionOne.vue
+++ src/components/SectionOne.vue
@@ -3,6 +3,7 @@
      <div id="slider"></div>
      <div id="littleyellow">
         <h2>Do You Have A Construction Project We Can Help You With?</h2>
+        <div id="buttoncontainer"><button>GET A FREE QUOTE</button></div>
      </div>
   </div>
 </template>
@@ -11,9 +12,13 @@
 export default {
 
 }
+
 </script>
 
 <style scoped>
+h2{
+  margin-bottom:1.2rem;
+}
 #slider{
     background-image: url('../assets/home-slider-62528242.jpg');
     background-size: cover;
@@ -27,6 +32,22 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
 }
+button{
+  color: white;
+  background-color: rgba(49,49,47,255);
+  border: .15rem solid white;
+  padding: 1rem 2rem 1rem 2rem;
+  position:absolute;
+  top: 5.5rem;
+  left:40rem;
+}
+button:hover{
+  color: rgba(49,49,47,255);
+  background-color: white;
+  border: .15rem solid rgba(49,49,47,255);
+}
+
 
 </style>
